ef3ff4131a21448d2aa46cd5569ccc2cb06ee3ea
Krishna Indoria <indoriayt5@gmail.com>
2026-01-11 15:35:52 +0530

    Live API issue solved

---
+++ src/Utils/Rest.js
@@ -0,0 +1,154 @@
+export const Recommended = [
+    {
+"id": "72273333",
+"name": "Margherita",
+"category": "Veg Pizza",
+"description": "Pizza topped with our herb-infused signature pan sauce and mozzarella cheese. A classic treat for all cheese lovers out there! (PAN Per/Med-292 Kcal/100g | TnC-293 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/41b56b0c-c864-4081-8c9c-ed4585cb3a5c_e725dfb8-341d-4ef3-9914-e7a2634187aa.jpg",
+    },
+
+    {
+"id": "72273361",
+"name": "Veggie Feast",
+"category": "Veg Pizza",
+"description": "Serves 1 | Herbed onion and green capsicum, juicy sweet corn & mozzarella cheese with flavourful pan sauce (PAN Per/Med-269 Kcal/100g | TnC-274 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/185b654c-4ff2-42ca-991e-11caf0781cb5_9fd71142-80db-4471-bec2-f80db3b1e08f.jpg",
+    },
+
+    {
+"id": "72273294",
+"name": "Tandoori Paneer",
+"category": "Veg Pizza",
+"description": "Serves 1 | It's our signature. Spiced paneer, crunchy onions & green capsicum, spicy red paprika with delicious tandoori sauce and mozzarella cheese! (PAN Per/Med-335 Kcal/100g | TnC-326 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/413649b2-52b2-42d8-afcf-fff2174192b2_71809375-48ba-48f6-bdba-b464956098bb.jpg",
+    },
+
+    {
+"id": "72273263",
+"name": "Country Feast",
+"category": "Veg Pizza",
+"description": "Serves 1 | Loaded with herbed onion & green capsicum, sweet corn, tomato with signature pan sauce and mozzarella cheese. It's a feast you can't refuse (PAN Per/Med-244 Kcal/100g | TnC-282 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/40b16d1c-c89d-4938-8a34-031e6792ee0f_3b7f3b2b-cd20-47c0-b68e-9740ee2c3c6a.jpg",
+    },
+
+ {
+"id": "111819621",
+"name": "Veggie Supreme",
+"category": "Veg Pizza",
+"description": "Serves 1 | A supreme combination of black olives, green capsicum, mushroom, onion, spicy red paprika & juicy sweet corn with flavourful pan sauce and mozzarella cheese. (PAN Per/Med-254 Kcal/100g | TnC-258 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/497f5159-0ffd-4f0f-ba40-9411e04bf4c1_80ec6a1d-5289-4d2c-b143-74e57132937a.jpg",
+ },
+
+ {
+"id": "72273341",
+"name": "Chicken Sausage",
+"category": "Non Veg Pizza",
+"description": "Serves 1 | Pizza topped with chicken n cheese sausages & crunchy onions, flavourful pan sauce and mozzarella cheese (PAN Per/Med-310 Kcal/100g | TnC-312 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza| Stuff Crust Chicken Seekh Kebab Add : Per: 389 kcal/Pizza | Med: 614 Kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2025/4/16/4b4ded4e-1e01-4d35-bebd-b055a22cb43a_2b6ef11b-6134-4744-87d5-9f49a9042d50.jpg_compressed",
+ },
+
+ {
+"id": "72273167",
+"name": "Murg Malai Chicken",
+"category": "Non Veg Pizza",
+"description": "Serves 1 | Amazingly flavourful chicken malai tikka, herbed onion & green capsicum, spicy red paprika with flavourful pan sauce and mozzarella cheese (PAN Per/Med-269 Kcal/100g | TnC-299 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza| Stuff Crust Chicken Seekh Kebab Add : Per: 389 kcal/Pizza | Med: 614 Kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/0727a66a-07ce-4ff6-8ffb-ec2d070aab85_055da41f-a59a-49a2-8524-975e3f59d6d7.jpg",
+ },
+
+ {
+"id": "87270853",
+"name": "Chicken Supreme",
+"category": "Non Veg Pizza",
+"description": "Serves 1 | Loaded with delicious chicken tikka, flavourful herbed chicken, spicy schezwan chicken meatball with flavourful pan sauce and mozzarella cheese. (PAN Per/Med-273 Kcal/100g | TnC-284 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza| Stuff Crust Chicken Seekh Kebab Add : Per: 389 kcal/Pizza | Med: 614 Kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/9cb9383d-5776-450a-91c9-51cd839c55b1_6eed4f34-f7a7-4f67-abfa-649de5903176.jpg",
+ },
+
+ {
+"id": "72273104",
+"name": "Triple Chicken Feast",
+"category": "Non Veg Pizza",
+"description": "Serves 1 | Spicy schezwan chicken meatball, flavourful herbed chicken, juicy chicken sausage, green capsicum & onion, spicy red paprika with classic pan sauce and mozzarella cheese (PAN Per/Med-262 Kcal/100g | TnC-277 Kcal/100g | Stuffed Crust Add : Per: 295 Kcal/Pizza | Med: 448kcal/Pizza| Ultimate Cheese : PER: 250 kcal/Pizza | MED: 375 kcal/Pizza| Stuff Crust Chicken Seekh Kebab Add : Per: 389 kcal/Pizza | Med: 614 Kcal/Pizza. Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/006ba8b5-ae01-468f-aa2b-0f29d5f18777_acb7a7c2-4f55-4792-894f-c31653f249c1.jpg",
+ },
+
+ {
+"id": "117545640",
+"name": "Classic Onion Capsicum",
+"category": "Flavour Fun Pizzas",
+"description": "Serves 1 | Pizza topped with our classic pan sauce, crunchy onion & capsicum and a flavourful dressing. (307 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/2/43d430bb-cf34-4230-b88e-8784728fd3ef_06fd1528-17e2-418e-bc67-d7e7b8e493bf.jpg",
+ },
+
+ {
+"id": "133110371",
+"name": "Cheesy Spicy Delight",
+"category": "Flavour Fun Pizzas",
+"description": "Pizza topped with mozzarella cheese, a flavourful dressing, onion and spicy green chilli, sprinkled with our signature spiced seasoning. (367 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/2/78fc6391-10f2-45b3-afd4-416a3bfb8260_dc58c551-a663-4499-b951-99b287116ced.jpg",
+ },
+
+ {
+"id": "133110378",
+"name": "Chilli Paneer Sizzle",
+"category": "Flavour Fun Pizzas",
+"description": "Pizza topped with spiced paneer, a spicy Schezwan sauce, juicy tomato, crunchy onion & capsicum and a flavourful dressing. (298 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/ee217a6b-423d-4cab-a98f-edec542c7103_563a38ad-b7e7-4e49-bcf0-c55a49a980b9.jpg",
+ },
+
+{
+"id": "133110384",
+"name": "Classic Chicken Pepperoni & Onion",
+"category": "Flavour Fun Pizzas",
+"description": "Pizza topped with our classic signature pan sauce, chicken pepperoni, crunchy onion and a flavourful dressing. (332 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/2/e6f4237a-55ab-46a4-adbe-cbf79541416f_ef441d3e-0f31-4c12-b114-08a564de7ee9.jpg",
+},
+
+{
+"id": "133110382",
+"name": "Classic Herbed Chicken & Capsicum",
+"category": "Flavour Fun Pizzas",
+"description": "Pizza topped with our classic signature pan sauce, herbed chicken, crunchy capsicum and a flavourful dressing. (331 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/2/4bd67a36-e254-489a-9519-3118951e5b11_1a56a4d2-72dd-46cc-aa17-8c806f086c91.jpg",
+},
+
+{
+"id": "133110380",
+"name": "Chatpata Chicken Feast",
+"category": "Flavour Fun Pizzas",
+"description": "Pizza topped with a spicy tandoori sauce, Schezwan chicken meatballs, crunchy onion & capsicum, green chilli, juicy tomato and a flavourful dressing. (294 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/2/d0247064-0689-49fc-b382-fed753d47f28_61e83c62-d60d-456b-aa27-b83f2f89082a.jpg",
+},
+
+{
+"id": "114788348",
+"name": "Sprinkled Fries - New",
+"category": "Appetizers",
+"description": "Serves 1 | Baked fries seasoned with our signature peruvian seasoning (204 Kcal/100g) Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/ec0a4db2-58c1-4b6a-ad52-e8c1ff2d5e70_cced7c63-5625-42ce-9456-8ab71948f3f9.jpg",
+},
+
+{
+"id": "70601554",
+"name": "Spicy Baked Chicken Wings 4pc",
+"category": "Appetizers",
+"description": "Serves 1 | 4 pcs of tender and juicy sweet chilly chicken wings (189 Kcal/100g) Contains Cereals containing Gluten (Wheat) & Soya.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/8e5ecbd1-b453-413f-8429-57331f004c65_c389fbb2-46c1-4060-8960-8da8a2232ae6.jpg",
+},
+
+{
+"id": "119747071",
+"name": "Brow-wow-nie",
+"category": "Drinks & Desserts",
+"description": "Serves 1 | A divine choco brownie for your all time chocolate cravings (273 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/21e66abc-3438-490f-8250-f69bd7a146a7_3a54cd83-2de8-4e0d-8ade-a1d796acc6cb.jpg",
+},
+
+{
+"id": "70601515",
+"name": "Choco Volcano",
+"category": "Drinks & Desserts",
+"description": "Serves 1 | Warm choco cake with gooey center (262 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
+"imageId": "FOOD_CATALOG/IMAGES/CMS/2026/1/5/dcfeee5a-cf20-4449-b265-2ddc573d681d_9b0679c8-6fa3-481d-8955-b4b22a3934cd.jpg",
+}
+
+]
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,32 +1,25 @@
-import { useState,useEffect } from "react";
-import { useParams } from "react-router";
-import MenuCard from "./MenuCard";
-
+import { Recommended } from "../Utils/Rest";
 export default function RestaurantMenu(){
-    const {id} = useParams();
-    const [RestData,setRestData] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-          const proxyServer = "https://cors-anywhere.herokuapp.com/";
-          const swiggyServer = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9753&lng=77.591&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`;
-    
-          const response = await fetch(proxyServer + swiggyServer);
-          const data = await response.json();
-          const tempData = data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
-          const filterData = tempData.filter((item)=> 'title' in item?.card?.card)
-          setRestData(filterData); 
-    
-        }
-    
-        fetchData();
-      }, []);
+  return(
+    <div className="w-[65%] mx-auto mt-10">
+      <h2 className="font-bold text-3xl mb-7">Recommended</h2>
+      {
+      Recommended.map((item)=>{
+        return(
+        <div key={item.id} className="w-full flex justify-between my-5">
+          <div className="w-[70%]">
+            <p className="font-bold text-xl">{item.name}</p>
+            <p>{item.description}</p>
+          </div>
 
-      return(
-        <div className="w-[80%] mx-auto">
-          {
-          RestData.map((MenuItems)=> <MenuCard key={MenuItems?.card?.card?.title} MenuItems={MenuItems?.card?.card}></MenuCard>)
-          }
+          <div>
+            <img className="object-cover h-36 w-39 rounded-2xl" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+item.imageId}></img>
+            <button className="relative bottom-6 left-5 bg-white rounded-2xl text-green-600 text-[20px] font-bold border-2 border-gray-300 w-30 h-10 transform hover:bg-gray-300 hover:cursor-pointer">ADD</button>
+          </div>
         </div>
-      )
-    
+        )
+      })
+    }
+    </div>
+  )
 }
